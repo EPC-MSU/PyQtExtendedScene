2f2e1d5f9116bff7057c6dd923ec4b5a417634a5
timerke <timerke@mail.ru>
2024-09-13 10:53:06 +0300

    Added docstrings for Sender generator

--- PyQtExtendedScene/sender.py
+++ PyQtExtendedScene/sender.py
@@ -1,19 +1,29 @@
+from typing import Any, Callable
 from PyQt5.QtCore import pyqtSignal, QObject
 
 
-def get_signal_sender(value_type: type):
+def get_signal_sender(value_type: type) -> type:
     """
-    :param value_type:
+    :param value_type: the type of values that the signal will need to send.
+    :return: class that sends values of a given type.
     """
 
     class Sender(QObject):
 
         _signal: pyqtSignal = pyqtSignal(value_type)
 
-        def connect(self, func) -> None:
+        def connect(self, func: Callable[..., Any]) -> None:
+            """
+            :param func: callback function that should receive values when sent via a signal.
+            """
+
             self._signal.connect(func)
 
         def emit(self, value) -> None:
+            """
+            :param value: value to send.
+            """
+
             self._signal.emit(value)
 
     return Sender
