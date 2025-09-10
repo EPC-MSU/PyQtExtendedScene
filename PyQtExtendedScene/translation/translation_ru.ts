b00873219c99f06f92432e0e19bbc1ae9314a872
timerke <timerke@mail.ru>
2025-09-10 16:35:30 +0300

    Generated translation in Russian

--- PyQtExtendedScene/translation/translation_ru.ts
+++ PyQtExtendedScene/translation/translation_ru.ts
@@ -8,7 +8,7 @@
         <translation>Добавить точку&#x09;Shift+Right-click</translation>
     </message>
     <message>
-        <location filename="../extendedscene.py" line="198"/>
+        <location filename="../extendedscene.py" line="199"/>
         <source>Rotate selected elements 90° clockwise</source>
         <translation>Повернуть выделенные элементы на 90° по часовой стрелке</translation>
     </message>
