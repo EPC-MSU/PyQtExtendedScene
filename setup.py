794cdb075dddc73fba4ef63fba411c8edf481dde
timerke <timerke@mail.ru>
2025-09-29 16:36:14 +0300

    Version increment

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as file:
     long_description = file.read()
 
 setup(name="PyQtExtendedScene",
-      version="2.0.1",
+      version="2.0.2",
       description="Extended scene library",
       long_description=long_description,
       long_description_content_type="text/markdown",
