8937b6e7be8753b8e01901df3e00c3b5b1b489cb
timerke <timerke@mail.ru>
2025-12-01 11:13:10 +0300

    Fixed setup.py to install on different Python versions #126867

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 
 with open("README.md", "r") as file:
@@ -13,14 +13,15 @@ setup(name="PyQtExtendedScene",
       author="EPC MSU",
       author_email="mihalin@physlab.ru",
       license="MIT",
-      packages=["PyQtExtendedScene"],
+      packages=find_packages(),
+      python_requires=">=3.6",
       install_requires=[
-            "PyQt5",
+            'PyQt5>=5.8.2, <=5.15.0; python_version=="3.6"',
+            'PyQt5; python_version>"3.6"',
       ],
       classifiers=[
             "Programming Language :: Python :: 3",
             "License :: OSI Approved :: MIT License",
             "Operating System :: OS Independent",
       ],
-      python_requires=">=3.6",
       zip_safe=False)
