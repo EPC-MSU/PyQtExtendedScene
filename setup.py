1661957022bec9d45c876667216c4c9cd2215e42
timerke <timerke@mail.ru>
2026-05-25 17:15:20 +0300

    Version increment

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 
 setup(name="PyQtExtendedScene",
-      version="2.0.2",
+      version="2.0.3",
       description="Extended scene library",
       url="https://github.com/EPC-MSU/PyQtExtendedScene",
       author="EPC MSU",
